cd246b846267194f399222c052dee26225550781
khalilkrugerOS <arabickhalil@gmail.com>
2025-10-27 08:09:42 +0100

    added resend for email

--- lib/env.ts
+++ lib/env.ts
@@ -8,6 +8,7 @@ export const env = createEnv({
     AUTH_GOOGLE_CLIENT_SECRET: z.string().min(1),
     BETTER_AUTH_URL: z.string().url().default("http://localhost:3000"),
     BETTER_AUTH_SECRET: z.string().min(1),
+    RESEND_API_KEY: z.string().min(1),
   },
 
   /**
