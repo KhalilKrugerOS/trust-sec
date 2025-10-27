1c6622714bf24588bdf57b20a18cb034472b4106
khalilkrugerOS <arabickhalil@gmail.com>
2025-10-27 08:12:54 +0100

    added email signin

--- app/(auth)/login/_components/LoginForm.tsx
+++ app/(auth)/login/_components/LoginForm.tsx
@@ -1,84 +1,129 @@
 "use client"; // bc of useTransition
 
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Loader, Mail } from "lucide-react";
+import { Loader, Mail, Send } from "lucide-react";
 import { authClient } from "@/lib/auth-client";
 import { toast } from "sonner";
-import { useTransition } from "react";
+import { useState, useTransition } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LoginForm() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [isGoogleAuthPending, startGoogleAuthTransition] = useTransition();
+  const [isEmailAuthPending, startEmailAuthTransition] = useTransition(); // use Transition is useful for pending states
 
-    const [isGoogleAuthPending, startGoogleAuthTransition] = useTransition();
-
-    async function handleGoogleSignIn() {
-
-        startGoogleAuthTransition(async () => {
-            await authClient.signIn.social({
-                provider: "google",
-                callbackURL: "/courses", // Redirect after successful login
-                fetchOptions: {
-                    onSuccess: () => {
-                        toast.success("Successfully signed in with Google!");
-                    },
-                    onError: (error) => {
-                        toast.error("Failed to sign in with Google.", { description: error.error.message });
-                    }
-                }
+  async function handleGoogleSignIn() {
+    startGoogleAuthTransition(async () => {
+      await authClient.signIn.social({
+        provider: "google",
+        callbackURL: "/courses", // Redirect after successful login
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Successfully signed in with Google!");
+          },
+          onError: (error) => {
+            toast.error("Failed to sign in with Google.", {
+              description: error.error.message,
             });
-        });
-    };
-    return (
-        <Card className="w-full backdrop-blur-xl bg-card/80 dark:bg-trustsec-widget/80 border-trustsec-3/30 shadow-2xl">
-            <CardHeader>
-                <CardTitle className="text-xl"> Welcome Back </CardTitle>
-                <CardDescription>Login With Email</CardDescription>
-            </CardHeader>
+          },
+        },
+      });
+    });
+  }
 
-            <CardContent className="flex flex-col gap-4">
-                <Button
-                    disabled={isGoogleAuthPending} // Disable button while auth is pending
-                    className="w-full" 
-                    variant="outline"
-                    onClick={handleGoogleSignIn}
-                >
-                    { isGoogleAuthPending ? 
-                        <>
-                           <Loader className="size-4 animate-spin" />
-                        </> : 
-                        <>  
-                            <Mail className="w-4 h-4" />
-                            Sign in with Google
-                        </>
-                    }
-                </Button>
-                <div className="relative text-center text-sm after:absolute
-                after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+  function handleEmailSignIn() {
+    startEmailAuthTransition(async () => {
+      await authClient.emailOtp.sendVerificationOtp({
+        email,
+        type: "sign-in",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Verification email sent!", {
+              description: `Check your inbox for a verification link.`,
+            });
+            // setEmail(""); // Optionally clear email input
+            router.push(`/verify-email?email=${encodeURIComponent(email)}`);
+          },
+          onError: (error) => {
+            toast.error("Failed to send verification email.", {
+              description: error.error.message,
+            });
+          },
+        },
+      });
+    });
+  }
 
-                    <span className="relative text-muted-foreground z-10 bg-card px-2">
-                        Or continue with
-                    </span>
-                </div>
+  return (
+    <Card className="w-full backdrop-blur-xl bg-card/80 dark:bg-trustsec-widget/80 border-trustsec-3/30 shadow-2xl">
+      <CardHeader>
+        <CardTitle className="text-xl"> Welcome Back </CardTitle>
+        <CardDescription>Login With Email</CardDescription>
+      </CardHeader>
 
-                <div className="grid gap-3">
-                    <div className="grid gap-2">
-                        <Label htmlFor="email">
-                            Email
-                        </Label>
-                        <Input
-                            type="email"
-                            placeholder="flex.fouleni@exmaple.com"
-                            />
-                    <Button className="w-full">
-                        Continue with Email
-                    </Button>     
-                    </div>
-                   
-                </div>
+      <CardContent className="flex flex-col gap-4">
+        <Button
+          disabled={isGoogleAuthPending} // Disable button while auth is pending
+          className="w-full"
+          variant="outline"
+          onClick={handleGoogleSignIn}
+        >
+          {isGoogleAuthPending ? (
+            <>
+              <Loader className="size-4 animate-spin" />
+            </>
+          ) : (
+            <>
+              <Mail className="w-4 h-4" />
+              Sign in with Google
+            </>
+          )}
+        </Button>
+        <div
+          className="relative text-center text-sm after:absolute
+                after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border"
+        >
+          <span className="relative text-muted-foreground z-10 bg-card px-2">
+            Or continue with
+          </span>
+        </div>
 
-            </CardContent>
-        </Card >
-    )
+        <div className="grid gap-3">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              type="email"
+              placeholder="flen.fouleni@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            <Button
+              className="w-full"
+              onClick={handleEmailSignIn}
+              disabled={isEmailAuthPending}
+            >
+              {isEmailAuthPending ? (
+                <Loader className="size-4 animate-spin" />
+              ) : (
+                <>
+                  <Send className="w-4 h-4" /> <span>Continue with Email</span>
+                </>
+              )}
+            </Button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
 }
---
+++ app/(auth)/verify-email/page.tsx
@@ -0,0 +1,100 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { authClient } from "@/lib/auth-client";
+import { Loader2 } from "lucide-react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState, useTransition } from "react";
+import { toast } from "sonner";
+
+export default function VerifyRequestPage() {
+  const [otp, setOtp] = useState("");
+  const [isOtpPending, startOtpTransition] = useTransition();
+  const params = useSearchParams();
+  const email = params.get("email");
+  const router = useRouter();
+
+  function verifyEmail() {
+    console.log("Verifying email with OTP:", otp, "for email:", email);
+    startOtpTransition(async () => {
+      authClient.signIn.emailOtp({
+        otp: otp,
+        email: email as string,
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Email verified successfully!");
+            router.push("/courses");
+          },
+          onError: (error) => {
+            toast.error("Failed to verify email.", {
+              description: error.error.message,
+            });
+            // TODO: Optionally clear OTP input
+          },
+        },
+      });
+    });
+  }
+  return (
+    <Card className="w-full mx-auto space-y-6">
+      <CardHeader className="text-center">
+        <CardTitle className="text-2xl mb-2">Check your email</CardTitle>
+        <CardDescription>
+          We have sent a verification link to your email address. Please check
+          your inbox and click on the link to verify your account.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="mx-auto text-center">
+        <div className="flex flex-col items-center space-y-2">
+          <InputOTP
+            maxLength={6}
+            value={otp}
+            onChange={setOtp}
+            className="gap-2"
+          >
+            <InputOTPGroup>
+              <InputOTPSlot index={0} />
+              <InputOTPSlot index={1} />
+              <InputOTPSlot index={2} />
+            </InputOTPGroup>
+            <InputOTPSeparator />
+            <InputOTPGroup>
+              <InputOTPSlot index={3} />
+              <InputOTPSlot index={4} />
+              <InputOTPSlot index={5} />
+            </InputOTPGroup>
+          </InputOTP>
+          <p className="mt-4 text-sm text-muted-foreground">
+            enter the 6-digit code we just emailed you.
+          </p>
+          <Button
+            className="mt-6 w-full"
+            onClick={verifyEmail}
+            disabled={isOtpPending || otp.length < 6}
+          >
+            {isOtpPending ? (
+              <>
+                <Loader2 className="size-4 animate-spin" />
+                <span> Verifying...</span>
+              </>
+            ) : (
+              "Verify Account"
+            )}
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
